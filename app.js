94d8e070f37d9d801c4d02441bcdc23932bb498a
Mirsaid <xushmir1203@gmail.com>
2024-12-09 21:54:58 +0500

    first

---
+++ app.js
@@ -0,0 +1,13 @@
+let ism = prompt('ismingizni kiriting')
+
+let firstLetter = ism.charAt().toUpperCase()
+
+let theEnd = ism.slice(1).toLowerCase()
+
+let result = firstLetter + theEnd
+
+alert(`Salom ${result}`)
+
+for (let i=0; i<10; ){
+    console.log('Mirsaid')
+}
